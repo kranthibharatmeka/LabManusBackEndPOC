6d3ddc39d48367801a4cd25c5925fbfab8ecc405
Kranthi Meka <kranthibharatmeka@gmail.com>
2019-08-26 21:26:57 +0100

    - Added code to retrieval the User Form for Customisation.

--- src/main/java/com/lab/manus/controller/FormsRetrievalController.java
+++ src/main/java/com/lab/manus/controller/FormsRetrievalController.java
@@ -5,10 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.lab.manus.entity.FormEntities;
 import com.lab.manus.entity.FormEntity;
 import com.lab.manus.entity.SubFormNames;
 import com.lab.manus.service.FormService;
@@ -19,18 +19,33 @@ public class FormsRetrievalController {
 	@Autowired
 	FormService formService;
 
-	@RequestMapping(value="/getAllFormNames", method=RequestMethod.GET)
+	@GetMapping(value="/getAllFormNames")
 	public String getAllForms(Model model) {
 		List<SubFormNames> allFormNames = formService.getAllFormNames();
 		model.addAttribute("allFormNames", allFormNames);		
 		return "ShowAllFormNames";
 	}
 	
-	@RequestMapping(path="/getForm/{form_id}", method=RequestMethod.GET)
+	@GetMapping(value="/getAllFormForEdit")
+	public String getAllFormsForEdit(Model model) {
+		List<SubFormNames> allFormNames = formService.getAllFormNames();
+		model.addAttribute("allFormNames", allFormNames);		
+		return "ShowAllFormsForEdit";
+	}
+	
+	@GetMapping(path="/getForm/{form_id}")
 	public String getRequestedForm(Model model, @PathVariable("form_id") Long formId) {
 		List<FormEntity> requestedFormFieldsByFormId = formService.getFormFieldsByFormId(formId);
 		model.addAttribute("requestedFormFields", requestedFormFieldsByFormId);		
 		return "ShowRequestedForm";
+	}
+	
+	@GetMapping(path="/getFormInEditMode/{form_id}")
+	public String getFormInEditMode(Model model, @PathVariable("form_id") Long formId, FormEntities formEntities, FormEntity formEntity, SubFormNames subFormNames) {
+		List<FormEntity> requestedFormFieldsByFormId = formService.getFormFieldsByFormId(formId);
+		formEntities.setFormEntityList(requestedFormFieldsByFormId);
+		model.addAttribute("formEntities", formEntities);
+		return "DynamicForm";
 	}
 	
 }
